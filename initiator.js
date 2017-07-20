7d5a955160ce3a973975b18ca130ea57bcfdbd17
Henry Lin <henry.lin@ericsson.com>
2017-07-20 14:27:02 +0800

    split files

---
+++ initiator.js
@@ -0,0 +1,29 @@
+function getAccessToken(context){
+  var parameters = {
+    grant_type: "client_credential",
+    appid: "wx06b3b86f63137f4f",
+    secret: "15faf94a01a96db4567af88de17ae998"
+  };
+
+  var parametersAsString = qs.stringify(parameters);
+  var url = "https://api.weixin.qq.com/cgi-bin/token?" + parametersAsString;
+
+  https.get(url, (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+
+    res.on('data', (data) => {
+      var result = JSON.parse(data);
+      console.log("access token:", result.access_token);
+      context.accessToken = result.access_token;
+    });
+
+  }).on('error', (e) => {
+    console.error(e);
+  });
+};
+
+module.exports = function(context){
+  getAccessToken(context);
+  setInterval(getAccessToken, 7100 * 1000, context);
+};
--- server.js
+++ server.js
@@ -9,36 +9,9 @@ app.set('view engine', 'html');
 app.engine( '.html', require( 'ejs' ).__express );
 
 var context = {};
-getAccessToken(context);
-setInterval(getAccessToken, 7100 * 1000, context);
+require('./initiator')(context);
+require('./router')(app, context);      //router config file
 
-require('./index')(app);      //router config file
 server.listen(80,function(){
 console.log('App start,port 80.');
 });
-
-function getAccessToken(context) {
-  var parameters = {
-    grant_type: "client_credential",
-    appid: "wx06b3b86f63137f4f",
-    secret: "15faf94a01a96db4567af88de17ae998"
-  };
-  
-  var parametersAsString = qs.stringify(parameters);
-  var url = "https://api.weixin.qq.com/cgi-bin/token?" + parametersAsString;
-
-  https.get(url, (res) => {
-    console.log('statusCode:', res.statusCode);
-    console.log('headers:', res.headers);
-
-    res.on('data', (data) => {
-      console.log('data', data);
-      var result = JSON.parse(data);
-      console.log("access token:", result.access_token);
-      context.accessToken = result.access_token;
-    });
-    
-  }).on('error', (e) => {
-    console.error(e);
-  });
-};
