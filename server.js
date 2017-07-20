88f874deca4f03a45c6dae5a64b6f00e4e102a2d
youngerlhy <bulhy@163.com>
2017-07-19 23:02:13 -0400

    store access_token

--- server.js
+++ server.js
@@ -7,31 +7,37 @@ server  = require('http').Server(app);
 app.set('views',__dirname);    // config view 
 app.set('view engine', 'html'); 
 app.engine( '.html', require( 'ejs' ).__express );
-var context = null;
+
+var context = {};
+getAccessToken(context);
 setInterval(getAccessToken, 7100 * 1000, context);
+
 require('./index')(app);      //router config file
 server.listen(80,function(){
 console.log('App start,port 80.');
 });
 
 function getAccessToken(context) {
-  var data = {
+  var parameters = {
     grant_type: "client_credential",
     appid: "wx06b3b86f63137f4f",
     secret: "15faf94a01a96db4567af88de17ae998"
   };
   
-  var content = qs.stringify(data);
-  var url = "api.weixin.qq.com/cgi-bin/token?" + content;
+  var parametersAsString = qs.stringify(parameters);
+  var url = "https://api.weixin.qq.com/cgi-bin/token?" + parametersAsString;
 
   https.get(url, (res) => {
     console.log('statusCode:', res.statusCode);
     console.log('headers:', res.headers);
   
-    res.on('data', (d) => {
-      process.stdout.write(d);
+    res.on('data', (data) => {
+      console.log('data', data);
+      var result = JSON.parse(data);
+      console.log("access token:", result.access_token);
+      context.accessToken = result.access_token;
     });
-  
+    
   }).on('error', (e) => {
     console.error(e);
   });
