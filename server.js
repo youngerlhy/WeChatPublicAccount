c41924b565f5f7a6700bfa0f0b9bba86955d20cc
Henry Lin <henry.lin@ericsson.com>
2017-07-25 14:00:50 +0800

    remove unrecognized char

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var app = express();
 ADMIN_TAG_NAME = "admin";
 
-app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, 'css')));
 
 server = require('http').Server(app);
 app.set('views', __dirname); // config view 
