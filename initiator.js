a0a3f4ec5d78537e869c19f6e0d6d8e9fce8df46
Henry Lin <henry.lin@ericsson.com>
2017-07-25 11:16:29 +0800

    format

--- initiator.js
+++ initiator.js
@@ -1,238 +1,267 @@
 var https = require('https');
 var qs = require('querystring');
+var ADMIN_TAG_NAME = "admin";
 
-function getAccessToken(callback){
-  var parameters = {
-    grant_type: "client_credential",
-    appid: "wx067aa7e646581331",
-    secret: "d26907f7504d80d5aea6305db84ec510"
-  };
+function getAccessToken(callback) {
+	console.log('Start to get access token ...');
 
-  var parametersAsString = qs.stringify(parameters);
-  var url = "https://api.weixin.qq.com/cgi-bin/token?" + parametersAsString;
+	var parameters = {
+		grant_type : "client_credential",
+		appid : "wx067aa7e646581331",
+		secret : "d26907f7504d80d5aea6305db84ec510"
+	};
 
-  https.get(url, (res) => {
-    console.log('statusCode:', res.statusCode);
-    console.log('headers:', res.headers);
+	var parametersAsString = qs.stringify(parameters);
+	var url = "https://api.weixin.qq.com/cgi-bin/token?" + parametersAsString;
 
-    res.on('data', (data) => {
-      var result = JSON.parse(data);
-      console.log("access token:", result.access_token);
-      context.accessToken = result.access_token;
-	  if (typeof callback == "function") {
-		callback();
-	  };
-    });
+	https.get(url, function(res) {
+		console.log('statusCode:', res.statusCode);
+		console.log('headers:\n', res.headers);
 
-  }).on('error', (e) => {
-    console.error(e);
-  });
+		res.on('data', function(data) {
+			var result = JSON.parse(data);
+			if (result.errcode && 0 != result.errcode) {
+				console.error(result.errmsg);
+				return;
+			}
+			console.log("access token:\n", result.access_token);
+			context.accessToken = result.access_token;
+			if (typeof callback == "function") {
+				callback();
+			}
+			;
+		});
+
+	}).on('error', function(e) {
+		console.error(e);
+	});
 };
 
-function queryTagIdAndCreateIfNotExist(tagName, accessToken){
-  console.log('Start to query if tag %s is created...', tagName);
-  
-  var options = {
-    hostname: 'api.weixin.qq.com',
-    port: 443,
-    path: "/cgi-bin/tags/get?access_token=" + accessToken,
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8'
-    }
-  };
+function queryTagIdAndCreateIfNotExist(tagName, callback) {
+	console.log('Start to query if tag %s is created ...', tagName);
 
-  var req = https.request(options, (res) => {
-    console.log('statusCode:', res.statusCode);
-    console.log('headers:', res.headers);
+	var options = {
+		hostname : 'api.weixin.qq.com',
+		port : 443,
+		path : "/cgi-bin/tags/get?access_token=" + context.accessToken,
+		method : 'GET',
+		headers : {
+			'Content-Type' : 'application/json; charset=utf-8'
+		}
+	};
 
-    res.on('data', (data) => {
-      var result = JSON.parse(data);
-      console.log("result:", result);
-      for (var each in result.tags) {
-          if (tagName === result.tags[each].name) {
-              console.log("tag already created, tagId = ", result.tags[each].id);
-              return;
-          }
-      }
-      console.log("tag not yet present");
-      creatTags(tagName, accessToken);
-    });
-  });
-  req.on('error', (e) => {
-    console.error(e);
-  });
-  req.end();
+	var req = https.request(options, function(res) {
+		console.log('statusCode:', res.statusCode);
+		console.log('headers:\n', res.headers);
+
+		res.on('data', function(data) {
+			var result = JSON.parse(data);
+			console.log("result:\n", result);
+			if (result.errcode && 0 != result.errcode) {
+				console.error(result.errmsg);
+				return;
+			}
+			for ( var each in result.tags) {
+				if (tagName === result.tags[each].name) {
+					console.log("tag already created, tagId = ",
+							result.tags[each].id);
+					putTagId(tagName, result.tags[each].id);
+					callback();
+					return;
+				}
+			}
+			console.log("tag not yet present");
+			creatTags(tagName, callback);
+		});
+	});
+	req.on('error', function(e) {
+		console.error(e);
+	});
+	req.end();
 };
 
-function creatTags(tagName, accessToken){
-  console.log('Start to create Tag ', tagName);
-  var postData = JSON.stringify({
-    "tag" : {
-      "name" : tagName
-    }
-  });
-  
-  var options = {
-    hostname: 'api.weixin.qq.com',
-    port: 443,
-    path: "/cgi-bin/tags/create?access_token=" + accessToken,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-      'Content-Length': Buffer.byteLength(postData)
-    }
-  };
+function creatTags(tagName, callback) {
+	console.log('Start to create Tag %s ...', tagName);
+	var postData = JSON.stringify({
+		"tag" : {
+			"name" : tagName
+		}
+	});
 
-  var req = https.request(options, (res) => {
-    console.log('statusCode:', res.statusCode);
-    console.log('headers:', res.headers);
+	var options = {
+		hostname : 'api.weixin.qq.com',
+		port : 443,
+		path : "/cgi-bin/tags/create?access_token=" + context.accessToken,
+		method : 'POST',
+		headers : {
+			'Content-Type' : 'application/json; charset=utf-8',
+			'Content-Length' : Buffer.byteLength(postData)
+		}
+	};
 
-    res.on('data', (data) => {
-      var result = JSON.parse(data);
-      console.log("result:", result);
-      console.log("tagId = ", result.tag.id);
-    });
-  });
-  req.on('error', (e) => {
-    console.error(e);
-  });
-  req.write(postData);
-  req.end();
+	var req = https.request(options, function(res) {
+		console.log('statusCode:', res.statusCode);
+		console.log('headers:\n', res.headers);
+
+		res.on('data', function(data) {
+			var result = JSON.parse(data);
+			console.log("result:\n", result);
+			if (result.errcode && 0 != result.errcode) {
+				console.error(result.errmsg);
+				return;
+			}
+			console.log("tagId = ", result.tag.id);
+			putTagId(tagName, result.tag.id);
+			callback();
+		});
+	});
+	req.on('error', function(e) {
+		console.error(e);
+	});
+	req.write(postData);
+	req.end();
 };
 
-function createDefaultButtons(accessToken){
-  console.log('Start to create default buttons...');
-  var postData = JSON.stringify({
-     button:[
-      {
-           name:"活动",
-           sub_button:[
-           {
-               type:"click",
-               name:"羽毛球报名",
-               url:"http://ec2-34-210-237-255.us-west-2.compute.amazonaws.com:80/",
-               key:"sign up"
-            }]
-       },{
-           name:"新闻",
-           sub_button:[
-           {
-               type:"click",
-               name:"News In BID",
-               url:"",
-               key:"News In BID"
-            },{
-               type:"click",
-               name:"Old News",
-               url:"",
-               key:"Old News"
-            }]
-       },{
-           name:"友链",
-           sub_button:[
-           {
-               type:"click",
-               name:"妥尼唠英文",
-               url:"",
-               key:"Tony English"
-            },{
-               type:"click",
-               name:"Leo Public Account",
-               url:"",
-               key:"Leo Public Account"
-            },{
-               type:"click",
-               name:"大车车",
-               url:"",
-               key:"Big Car"
-            }]
-       }]
-  });
-  
-  var options = {
-    hostname: 'api.weixin.qq.com',
-    port: 443,
-    path: "/cgi-bin/menu/create?access_token=" + accessToken,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-      'Content-Length': Buffer.byteLength(postData)
-    }
-  };
-
-  var req = https.request(options, (res) => {
-    console.log('statusCode:', res.statusCode);
-    console.log('headers:', res.headers);
-
-    res.on('data', (data) => {
-      var result = JSON.parse(data);
-      console.log("result:", result);
-    });
-  });
-  req.on('error', (e) => {
-    console.error(e);
-  });
-  req.write(postData);
-  req.end();
+function putTagId(tagName, tagId) {
+	if (!context.tags) {
+		context.tags = {};
+	}
+	eval("context.tags." + tagName + "=" + tagId);
 };
 
-function createConditionalButtons(accessToken){
-  console.log('Start to create conditional buttons...');
-  var postData = JSON.stringify({
-     button:[
-       {
-           name:"查询口令",
-           type:"click",
-           key:"query token"
-       },{
-           name:"发起报名",
-           type:"click",
-           key:"launch sign up"
-       },{
-           name:"结束报名",
-           type:"click",
-           key:"stop sign up"
-       }],
-     matchrule:{
-       tag_id:"2",
-     }
-  });
-  
-  var options = {
-    hostname: 'api.weixin.qq.com',
-    port: 443,
-    path: "/cgi-bin/menu/addconditional?access_token=" + accessToken,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-      'Content-Length': Buffer.byteLength(postData)
-    }
-  };
+function createDefaultButtons(callback) {
+	console.log('Start to create default buttons ...');
+	var postData = JSON
+			.stringify({
+				button : [
+						{
+							name : "活动",
+							sub_button : [ {
+								type : "click",
+								name : "羽毛球报名",
+								url : "http://ec2-34-210-237-255.us-west-2.compute.amazonaws.com:80/",
+								key : "sign up"
+							} ]
+						}, {
+							name : "新闻",
+							sub_button : [ {
+								type : "click",
+								name : "News In BID",
+								url : "",
+								key : "News In BID"
+							}, {
+								type : "click",
+								name : "Old News",
+								url : "",
+								key : "Old News"
+							} ]
+						}, {
+							name : "友链",
+							sub_button : [ {
+								type : "click",
+								name : "妥尼唠英文",
+								url : "",
+								key : "Tony English"
+							}, {
+								type : "click",
+								name : "Leo Public Account",
+								url : "",
+								key : "Leo Public Account"
+							}, {
+								type : "click",
+								name : "大车车",
+								url : "",
+								key : "Big Car"
+							} ]
+						} ]
+			});
 
-  var req = https.request(options, (res) => {
-    console.log('statusCode:', res.statusCode);
-    console.log('headers:', res.headers);
+	var options = {
+		hostname : 'api.weixin.qq.com',
+		port : 443,
+		path : "/cgi-bin/menu/create?access_token=" + context.accessToken,
+		method : 'POST',
+		headers : {
+			'Content-Type' : 'application/json; charset=utf-8',
+			'Content-Length' : Buffer.byteLength(postData)
+		}
+	};
 
-    res.on('data', (data) => {
-      var result = JSON.parse(data);
-      console.log("result:", result);
-    });
-  });
-  req.on('error', (e) => {
-    console.error(e);
-  });
-  req.write(postData);
-  req.end();
+	var req = https.request(options, function(res) {
+		console.log('statusCode:', res.statusCode);
+		console.log('headers:\n', res.headers);
+
+		res.on('data', function(data) {
+			var result = JSON.parse(data);
+			console.log("result:\n", result);
+			if (result.errcode && 0 != result.errcode) {
+				console.error(result.errmsg);
+				return;
+			}
+			callback();
+		});
+	});
+	req.on('error', function(e) {
+		console.error(e);
+	});
+	req.write(postData);
+	req.end();
 };
 
-function onTokenReady(){
-  queryTagIdAndCreateIfNotExist("主席", context.accessToken);
-  createDefaultButtons(context.accessToken);
-  //createConditionalButtons(context);
+function createConditionalButtons() {
+	console.log('Start to create conditional buttons ...');
+	var postData = JSON.stringify({
+		button : [ {
+			name : "发起报名",
+			type : "click",
+			key : "launch sign up"
+		}, {
+			name : "结束报名",
+			type : "click",
+			key : "stop sign up"
+		} ],
+		matchrule : {
+			tag_id : context.tags[ADMIN_TAG_NAME],
+		}
+	});
+
+	var options = {
+		hostname : 'api.weixin.qq.com',
+		port : 443,
+		path : "/cgi-bin/menu/addconditional?access_token="
+				+ context.accessToken,
+		method : 'POST',
+		headers : {
+			'Content-Type' : 'application/json; charset=utf-8',
+			'Content-Length' : Buffer.byteLength(postData)
+		}
+	};
+
+	var req = https.request(options, function(res) {
+		console.log('statusCode:', res.statusCode);
+		console.log('headers:\n', res.headers);
+
+		res.on('data', function(data) {
+			var result = JSON.parse(data);
+			console.log("result:\n", result);
+		});
+	});
+	req.on('error', function(e) {
+		console.error(e);
+	});
+	req.write(postData);
+	req.end();
 };
 
-module.exports = function(){
-  getAccessToken(onTokenReady);
-  setInterval(getAccessToken, 7100 * 1000);
+function createButtons() {
+	createDefaultButtons(createConditionalButtons);
+};
+
+function onTokenReady() {
+	queryTagIdAndCreateIfNotExist(ADMIN_TAG_NAME, createButtons);
+};
+
+module.exports = function() {
+	getAccessToken(onTokenReady);
+	setInterval(getAccessToken, 7100 * 1000);
 };
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var express = require("express");
-var path=require('path');
+var path = require('path');
 var app = express();
 app.use(express.static('css'));
 
-server  = require('http').Server(app);
-app.set('views',__dirname);    // config view 
-app.set('view engine', 'html'); 
-app.engine( '.html', require( 'ejs' ).__express );
+server = require('http').Server(app);
+app.set('views', __dirname); // config view 
+app.set('view engine', 'html');
+app.engine('.html', require('ejs').__express);
 
 context = {};
 require('./initiator')();
-require('./router')(app, context);      //router config file
+require('./router')(app); //router config file
 
-server.listen(80,function(){
-  console.log('App start,port 80.');
+server.listen(80, function() {
+	console.log('App start,port 80.');
 });
