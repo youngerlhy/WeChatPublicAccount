261d5130710d11f55f1fdd0fc99f97cad5701f42
Henry Lin <henry.lin@ericsson.com>
2017-07-27 10:05:20 +0800

    remove test code

--- createButtons.js
+++ createButtons.js
@@ -43,63 +43,6 @@ function createDefaultButtons(callback) {
 
 function createConditionalButtons() {
 	console.log('Start to create conditional buttons ...');
-	var buttonStatic = {
-	"button" : [
-		{
-			"name" : "活动",
-			"sub_button" : [ {
-				"type" : "view",
-				"name" : "羽毛球报名",
-				"url" : "http://ec2-34-210-237-255.us-west-2.compute.amazonaws.com:80/",
-				"key" : "sign up"
-			} ]
-		}, {
-			"name" : "新闻",
-			"sub_button" : [ {
-				"type" : "view",
-				"name" : "News In BID",
-				"url" : "http://www.baidu.com",
-				"key" : "News In BID"
-			}, {
-				"type" : "view",
-				"name" : "Old News",
-				"url" : "http://www.baidu.com",
-				"key" : "Old News"
-			} ]
-		}, {
-			"name" : "友链",
-			"sub_button" : [ {
-				"type" : "view",
-				"name" : "妥尼唠英文",
-				"url" : "http://www.baidu.com",
-				"key" : "Tony English"
-			}, {
-				"type" : "view",
-				"name" : "卡司游影",
-				"url" : "http://www.baidu.com",
-				"key" : "Leo Public Account"
-			}, {
-				"type" : "view",
-				"name" : "不务正业的大车车",
-				"url" : "http://www.baidu.com",
-				"key" : "Big Car"
-			} ]
-		},{
-			name : "管理",
-			sub_button : [{
-				name : "发起报名",
-				type : "click",
-				key : "launch sign up"
-			},{
-				name : "结束报名",
-				type : "click",
-				key : "stop sign up"
-			}]
-		} ],
-		matchrule: {
-			tag_id : context.tags[ADMIN_TAG_NAME],
-		}
-	};
 	var buttons = JSON.parse(fs.readFileSync(FILE));
 	buttons.button.push({
 		name : "管理",
