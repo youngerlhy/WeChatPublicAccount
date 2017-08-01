1914b54c226327b27e58c9999bed2a265811e2d5
EJMNNPW <xiao.c.li@ericsson.com>
2017-08-01 17:58:54 +0800

    fix mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -1,48 +1,56 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/testdb', {
-	useMongoClient : true,
-});
+var db = mongoose.createConnection();
+
 
 db.on('error', console.error.bind(console,'Database Connection error……'));
 
 db.on('open',function(callback){
-    console.log('db service connected.');
+	console.log('db service connected.');
 });
 
 db.on('connecting',()=>{
-    console.log('db connecting……');
+	console.log('db connecting……');
 });
 
 db.on('connected', ()=>{
-    console.log('db connected');
+	console.log('db connected');
 });
 db.on('disconnecting', ()=>{
-    console.log('db disconnecting...');
+	console.log('db disconnecting...');
 });
 db.on('disconnected', ()=>{
-    console.log('db disconnected');
+	console.log('db disconnected');
 });
 db.on('close', ()=>{
-    console.log('db close');
+	console.log('db close');
 });
+
+
+db.openUri('mongodb://127.0.0.1:27017/runoob');
 
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    nickname:{type:String},
-    imageurl:{type:String},
-    car:{type:Schema.Types.ObjectId, ref:'Car'},
-    gametype:[{type:Schema.Types.ObjectId, ref:'GameType'}]
+	wechatId:{type:String, unique:true},
+	nickname:{type:String},
+	imageurl:{type:String},
+	car:[{type:Schema.Types.ObjectId, ref:'Car'}],
+	gametype:[{type:Schema.Types.ObjectId, ref:'GameType'}]
 });
 var User = mongoose.model('User', UserSchema);
 
 
 var CarSchema = new Schema({
-    owner:{type:Schema.Types.ObjectId, ref:'User'},
-    passengers:[{type:Schema.Types.ObjectId, ref:'User'}],
-    available:{type:Boolean},
-    seatnum:{type:Number},
-    seatavailablenum:{type:Number},
+	owner:[{type:Schema.Types.ObjectId, ref:'User'}],
+	passengers:[{type:Schema.Types.ObjectId, ref:'User'}],
+	available:{type:Boolean},
+	seatnum:{type:Number},
+	seatavailablenum:{type:Number},
 });
 var Car = mongoose.model('Car', CarSchema);
 
@@ -72,58 +80,23 @@ var ScoreSchema = new Schema({
 var Score = mongoose.model('Score', ScoreSchema);
 
 var LogSchema = new Schema({
-    time:{type:Date},
-    log:{type:String}
+	time:{type:Date},
+	log:{type:String}
 });
 var Log = mongoose.model('Log', LogSchema);
 
-exports.insertSignupData = function(nickName, imageUrl, seatnum) {
-	var person = new User({
-		nickname : nickName,
-		imageurl : imageUrl
-	});
-
-if (seatnum > 0) {
-    var car = new Car({
-      owner : person,
-      available : true,
-      seatnum : seatnum,
-      seatavailablenum : seatnum - 1 
-	});
-	person.car = car;
-	car.save(function(err) {
-	if (err) {
-	    return console.log(err);
-	}
-    });
-}	
-person.save(function(err) {
-    if (err) {
-    	console.log(err);}
-    });
-}
-
-exports.insertPublishGame = function(startTime,endTime) {
-  var game = new Game({
-    startTime:startTime,
-    endTime:endTime
-  }) ;
-  game.save(function(err) {
-    if (err) {
-      return console.log(err);
-  }
-  });
-}
-exports.countUserByName = function(name, callback) {
-	User.count({nickname:name}, callback);
-}
-
 exports.findUserByName = function(name, callback){
-	User.findOne({nickname:name}, callback);
+	User.findOne({name:name}, function(err, obj){
+		callback(err, obj);
+	});
 }
 
-exports.findCarByName = function(name, callback){
+exports.findCarByName = function(name,callback){
+	User.findOne({name:name}).populate({path:'Car'}).exec(function(err, obj){
+		callback(err, obj);
+	});
 }
+
 
 exports.deleteUserCar = function(name){
     User.findOne({nickname: name}).then(function(user){
@@ -132,24 +105,107 @@ exports.deleteUserCar = function(name){
 });
 }
 
-exports.updateUserCar = function(name, callback){
+exports.updateUserCar = function(name,userdata,cardata,callback){
+	var updateStr = {$set:{userdata}};
+	User.update({name:name}, updateStr, function(err, result){
+		if(err){
+			console.log("Error:"+err);
+			return;
+		}else{
+			callback(result);
+		}
+	});
+	
+	if(cardata){
+		var user = User.findOne({name:name}, function(err, obj){
+			callback(err, obj);
+		});
+		updateStr = {$set:{cardata}};
+		Car.update({name:user.nickname}, updateStr, function(err, result){
+			if(err){
+				console.log("Error:"+err);
+				return;
+			}else{
+				callback(result);
+			}
+		});
+	}
+	
 	
 }
 
 exports.allotUserCar = function(){
+	var users = new Array();
+	users = User.find({}, function(err, result){
+		if(err){
+			console("Error:" + err);
+			return;
+		}else{
+			callback(result);
+		}
+	});
+	
+	var allotusers = new Array();
+	users.forEach(function(value, index, array){
+		if(value.car != null){   //
+			allotusers.push(value);
+		}
+	});
+	
+	var cars = new Array();
+	cars = Car.find({}, function(err, result){
+		if(err){
+			console("Error:" + err);
+			return;
+		}else{
+			callback(result);
+		}
+	});
+	
+	var seatnum = 0;
+	cars.forEach(function(value, index, array){
+		for(var i=0; i<cars[index].seatavailablenum; i++){
+			if(seatnum+i+1 <= allotusers.length){
+				cars[index].passengers = allotusers[seatnum+i];
+				cars[index].save(function(err){});				
+			}
+			// 若是座位数大于人数
+		}
+		seatnum += cars[index].seatavailablenum;
+	});
 }
 
-//for Test
+/**
+ * 再次发起报名时删除已有user和car
+ */
+exports.deleteAllUsersCars = function(){
+	Car.remove({}, function(err){
+		console.log("--------- Clean All Cars ---------");
+		if(err){
+			console.log("Clean all cars fail:"+err);
+		}else{
+			console.log("Clean all cars success.")
+		}
+		
+	});
+	User.remove({}, function(err){
+		console.log("--------- Clean All Users ---------");
+		if(err){
+			console.log("Clean all users fail:"+err);
+		}else{
+			console.log("Clean all users success.")
+		}
+	});
+}
 
-exports.test1 = function(){
-    console.log("This is the method test1");
-};
-
-//var mongoose = require("./mongoose");
-//mongoose.test1();
-
-
-//db.close();
-
-
-
+exports.insertPublishGame = function(startTime,endTime) {
+	  var game = new Game({
+	    startTime:startTime,
+	    endTime:endTime
+	  }) ;
+	  game.save(function(err) {
+	    if (err) {
+	      return console.log(err);
+	  }
+	  });
+}
