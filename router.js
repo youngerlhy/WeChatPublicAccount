25b738bd0325fad6ffb415e227574215e4883786
EJMNNPW <xiao.b.li@ericsson.com>
2017-08-07 16:43:31 +0800

    debug

--- router.js
+++ router.js
@@ -233,7 +233,7 @@ module.exports = function(app) {
 			    			    			  console.log("JSON:==="+json);
 			    			    			  
 			    			    			  var data = '[{"nickname":"Tiny Ding","imageurl":"http://wx.qlogo.cn/mmopen/vypzhLPqWka4cdIsQHWuU1IrztYcicz1icaibBW2rAoCbDFABK5TtLreFlnwvMbepkVgQDP7LcibcBbIicZ35bUEAbU5EjsCGUmAG/0","carname":"Phoenix"},{"nickname":"Phoenix","imageurl":"http://wx.qlogo.cn/mmopen/xJhQocZic7og1LicJVqXSc21aOPOUFDH0rBc3akeQkoU5kePONwWDKjmhqXv5W39rUKkHv83Uec3iaKPeZ5YZ8H2xqW4zueShRf/0","carname":"Phoenix"}]';
-			    			    			  res.render('sign_up_list', {data}); 
+			    			    			  res.json({data}); 
 			    						  }
 			    					  });
 		    					  }else{
