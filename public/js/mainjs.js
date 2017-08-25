acefae7288c2afbd0100818e0b980c3254a519e6
unknown <ekuadin@CN00202766.ericsson.se>
2017-08-25 16:11:44 +0800

    change index.html

---
+++ public/js/mainjs.js
@@ -0,0 +1,45 @@
+﻿window.fn = {};
+
+window.fn.open = function() {
+  var menu = document.getElementById('menu');
+  menu.open();
+};
+
+window.fn.load = function(page) {
+  var content = document.getElementById('content');
+  var menu = document.getElementById('menu');
+  content.load(page)
+    .then(menu.close.bind(menu));
+};
+
+window.fn.toggleMenu = function () {
+  document.getElementById('appSplitter').right.toggle();
+};
+
+window.fn.loadView = function (index) {
+  document.getElementById('appTabbar').setActiveTab(index);
+  document.getElementById('sidemenu').close();
+};
+
+window.fn.loadLink = function (url) {
+  window.open(url, '_blank');
+};
+
+window.fn.pushPage = function (page, anim) {
+  if (anim) {
+    document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title }, animation: anim });
+  } else {
+    document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title } });
+  }
+};
+
+var signUp = function() {
+  var username = document.getElementById('username').value;
+  var have_car = document.getElementById('have_car').value;
+	if (username) {
+		ons.notification.toast({message: '您已成功报名, Enjoy the game!', timeout: 3000});
+	} else {
+		ons.notification.alert('请输入姓名!');
+	}
+  
+};
